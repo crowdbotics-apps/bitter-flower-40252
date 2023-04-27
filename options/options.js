e4c7f5297f8eeadb732c4405d475ac54c4127da3
Crowdbotics <team@crowdbotics.com>
2023-04-27 13:53:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-flower-40252",
+  url: "https://bitter-flower-40252.botics.co",
+  api: "https://bitter-flower-40252.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
